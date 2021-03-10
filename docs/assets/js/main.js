e428d91ea966bf35e57b32dd229f698b165fb25c
Arthur Lui <luiarthur@gmail.com>
2021-03-10 09:26:40 -0800

    better katex and math demo

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,26 +1,4 @@
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js";
 import "https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"
-import "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js";
-import "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.min.js";
+import "./katex-setup.js"
 import "./disqus.js";
-
-// KaTeX
-$(document).ready(() => {
-  renderMathInElement(document.body, {
-    // ...options...
-    delimiters: [
-      { left: "$$", right: "$$", display: true },
-      { left: "$", right: "$", display: false },
-      { left: "\\[", right: "\\]", display: true }
-    ],
-    macros: {
-      "\\ind": "\\overset{ind}{\\sim}",
-      "\\iid": "\\overset{iid}{\\sim}",
-      "\\norm": "\\left\\lVert#1\\right\\rVert",
-      "\\p": "\\left(#1\\right)",
-      "\\bk": "\\left[#1\\right]",
-      "\\bc": "\\left\{#1\\right\\}",
-      "\\abs": "\\left|#1\\right|",
-    }
-  });
-});
--- docs/math-demo.md
+++ docs/math-demo.md
@@ -9,10 +9,19 @@ math: on
 
 This is how you [create github pages manually][1].
 
-And here is some math:
+And here is some display math:
 
 $$
 \int_0^\infty \theta e^{-\theta x}\, dx = 1
+$$
+
+Here is some inline math. $f(x) = \theta \exp\p{-\theta x}$.
+
+$$
+\begin{aligned}
+X_i &\ind \text{Exponential}(\theta) \\
+\bar{X} &\overset{\cdot}{\sim} \text{Normal}(\theta^{-1}, \theta^{-2}/n) \\
+\end{aligned}
 $$
 
 [1]: https://help.github.com/articles/creating-project-pages-manually
