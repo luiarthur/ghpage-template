ab6506eb26b34c7315a3b60e287506193fcaea6a
Arthur Lui <luiarthur@gmail.com>
2021-03-09 17:43:44 -0800

    updated look

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,4 +1,4 @@
-import "https://code.jquery.com/jquery-3.2.1.slim.min.js";
-import "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
+import 'https://code.jquery.com/jquery-3.5.1.min.js'
+// import "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
 import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js";
 import "./disqus.js";
