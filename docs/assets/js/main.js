120c2702f332a8773116369c5bdf23dd52e9f55e
Arthur Lui <luiarthur@gmail.com>
2021-03-09 16:46:09 -0800

    cleaned

---
+++ docs/assets/js/main.js
@@ -0,0 +1,4 @@
+import "https://code.jquery.com/jquery-3.2.1.slim.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
+import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js";
+import "./disqus.js";
